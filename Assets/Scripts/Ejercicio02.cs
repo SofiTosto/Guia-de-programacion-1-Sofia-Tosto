8dbc71a8bf39392424d3bf5e27bf7b664be162dd
SofiTosto <sofiatosto@gmail.com>
2022-04-03 18:04:22 -0300

    ejercicio 01 Terminado

--- Assets/Scripts/Ejercicio02.cs
+++ Assets/Scripts/Ejercicio02.cs
@@ -2,22 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ejercicio01 : MonoBehaviour
+public class Ejercicio02 : MonoBehaviour
 {
-
     // Start is called before the first frame update
     void Start()
     {
-        int num1;
-        int resultado;
-        num1 = 0;
-        num1 = num1 + 2;
-        Debug.Log(num1);
-        resultado = num1 * num1;
-        Debug.Log(resultado);
-     
-
-
+        
     }
 
     // Update is called once per frame
