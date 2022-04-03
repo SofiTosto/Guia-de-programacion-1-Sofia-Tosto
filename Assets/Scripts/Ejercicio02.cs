1102d3b6fa158b751ede9e3e13533584f5583e56
SofiTosto <sofiatosto@gmail.com>
2022-04-03 18:21:34 -0300

    Ejercicio 02 Terminado

--- Assets/Scripts/Ejercicio02.cs
+++ Assets/Scripts/Ejercicio02.cs
@@ -7,7 +7,17 @@ public class Ejercicio02 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        int num1;
+        int num2;
+        int resultadosuma;
+        int resultadomulti;
+        num1 = 4;
+        num2 = 5;
+        resultadosuma = num1 + num2;
+        Debug.Log (resultadosuma);
+        resultadomulti = num1 * num2;
+        Debug.Log (resultadomulti);
+
     }
 
     // Update is called once per frame
