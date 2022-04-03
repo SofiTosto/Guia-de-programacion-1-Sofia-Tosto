813391584be64f6a7b291775fe96618bb065620f
SofiTosto <sofiatosto@gmail.com>
2022-04-03 19:14:56 -0300

    Ejercicio 04 Terminado

---
+++ Assets/Scripts/Ejercicio04.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio04 : MonoBehaviour
+{
+    public int num1;
+    public int num2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int resultado;
+        resultado = num1 + num2;
+        Debug.Log("La suma entre " + num1 + " y " + num2 + " da como resultado " + resultado);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
