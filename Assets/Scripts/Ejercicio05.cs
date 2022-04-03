9ccede77bb6bfe680c8a0f4eccbe5f5b28189298
SofiTosto <sofiatosto@gmail.com>
2022-04-03 20:04:53 -0300

    Ejercicio 05 Terminado

---
+++ Assets/Scripts/Ejercicio05.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio05 : MonoBehaviour
+{
+    public int num1;
+    public int num2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int resultado;
+        if (num2 == 0)
+        {
+            Debug.Log("No se puede dividir por cero ");
+        }
+        else
+        {
+            resultado = num1 / num2;
+            Debug.Log(num1 + " / " + num2 + " = " + resultado);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
