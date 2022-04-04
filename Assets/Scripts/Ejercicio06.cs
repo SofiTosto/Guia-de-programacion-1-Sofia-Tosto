37804cacae1fa297d0f6148ca09be7c8f3c5e61e
SofiTosto <sofiatosto@gmail.com>
2022-04-03 21:32:43 -0300

    Ejercicio 05 Terminado

---
+++ Assets/Scripts/Ejercicio06.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio06 : MonoBehaviour
+{
+    public int num1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (num1 < 0)
+        {
+            Debug.Log("Ingresa un numero mayor a cero por favor");
+        }
+        if (num1 % 2 == 0)
+        {
+
+            Debug.Log("El numero ingresado es par");
+        }
+        else
+        {
+
+            Debug.Log("El numero ingresado es impar")
+        
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
